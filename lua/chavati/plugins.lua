b7131c10a3b3907128599881cbddb578af54edf6
Salvador Hernandez Lopez <chavatloz@gmail.com>
2021-03-30 18:01:33 -0600

    Plugin.lua

--- lua/chavati/plugins.lua
+++ lua/chavati/plugins.lua
@@ -13,7 +13,49 @@ vim.cmd('packadd packer.nvim')
 local packer = require'packer'
 local util = require'packer.util'
 
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
 
+
+
+return packer.startup(function()
+  local use = use
+  -- Style
+  use 'morhetz/gruvbox'
+  -- LSP Plugins
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-lua/completion-nvim'
+  use 'tjdevries/nlua.nvim'
+  use 'tjdevries/lsp_extensions.nvim'
+  -- Treesitter
+  use {'nvim-treesitter/nvim-treesitter',
+        cmd : ':TSUpdate'
+      }
+  use 'nvim-treesitter/playground'
+  -- Telescope
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-fzy-native.nvim'
+  -- NerdTree
+  -- Git
+  use 'tpope/vim-fugitive'
+  -- Surround
+  use 'tpope/vim-surround'
+  -- Auto Pairs
+  use 'jiangmiao/auto-pairs'
+  -- Tmux navigator
+  use 'christoomey/vim-tmux-navigator'
+  -- Latex
+  use 'lervag/vimtex'
+  -- Bufferline
+  -- React
+  use 'othree/vim-jsx'
+  --Prettier
+  use 'prettier/vim-prettier'
+  end
+)
 
 
 
