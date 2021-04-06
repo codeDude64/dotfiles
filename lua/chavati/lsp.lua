a4e191768e32475c1ed444af8d5bd3012d800640
Salvador Hernandez Lopez <chavatloz@gmail.com>
2021-04-06 13:09:24 -0500

    Gruvbox and lsp configuration

---
+++ lua/chavati/lsp.lua
@@ -0,0 +1,10 @@
+vim.g.gruvbox_contrast_dark = 'hard'
+
+if vim.fn.exists('+termguicolors') then
+  vim.g["&t_8f"] = "<Esc>[38;2;%lu;%lu;%lum]"
+  vim.g["&t_8b"] = "<Esc>[48;2;%lu;%lu;%lum]"
+end
+
+vim.g.gruvbox_invert_selection = '0'
+vim.g.colors_name = 'gruvbox'
+vim.g.background = 'dark'
