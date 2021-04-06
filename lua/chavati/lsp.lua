4c46e78c7cfb7827fbbe7fb812fa7facc0bfe017
Salvador Hernandez Lopez <chavatloz@gmail.com>
2021-04-06 13:26:07 -0500

    Add the mains lsp configuration

--- lua/chavati/lsp.lua
+++ lua/chavati/lsp.lua
@@ -1,3 +1,9 @@
+local lspconfig = require'lspconfig'
+local completion = require'completion'
+local lsp_status = require'lsp-status'
+
+
+-- Gruvbox and lsp
 vim.g.gruvbox_contrast_dark = 'hard'
 
 if vim.fn.exists('+termguicolors') then
@@ -7,3 +13,30 @@ end
 
 vim.g.gruvbox_invert_selection = '0'
 vim.cmd 'colorscheme gruvbox'
+
+
+lsp_status.register_progress()
+
+local function default_on_attach(client)
+  print('Attaching to ' .. client.name)
+  completion.on_attach(client)
+  lsp_status.on_attach(client)
+end
+
+local default_config = {
+  on_attach = default_on_attach,
+  capabilities = lsp_status.capabilities
+}
+
+lspconfig.tsserver.setup{ default_config }
+lspconfig.jsonls.setup{ default_config }
+lspconfig.cssls.setup{ default_config }
+lspconfig.html.setup{ default_config }
+lspconfig.omnisharp.setup{ default_config }
+lspconfig.intelephense.setup{ default_config }
+lspconfig.pyright.setup{ default_config }
+lspconfig.bashls.setup{ default_config }
+lspconfig.texlab.setup{ default_config }
+lspconfig.yamlls.setup{ default_config }
+lspconfig.vimls.setup{ default_config }
+lspconfig.dockerls.setup{ default_config }
