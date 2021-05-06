5334fe6e3cc93d1fb52de8cdd4b8c9f942390da4
Salvador Hernandez Lopez <chavatloz@gmail.com>
2021-05-05 19:04:16 -0500

    Change the order of the list of completion

--- lua/chavati/completion.lua
+++ lua/chavati/completion.lua
@@ -6,8 +6,8 @@ vim.g.diagnostic_enable_virtual_text = 1
 vim.g.diagnostic_insert_delay = 1
 vim.g.completion_chain_complete_list = {
   {complete_items = {'lsp', 'snippet'}},
-  {mode = '<c-p>'},
   {mode = '<c-n>'},
+  {mode = '<c-p>'},
 }
 
 vim.g.completion_enable_snippet = 'snippets.nvim'
