14ab7486f35195e15585683e781af01a2c52f39f
Salvador Hernandez Lopez <chavatloz@gmail.com>
2021-08-01 21:27:02 -0500

    Change the color scheme

--- lua/chavati/theme.lua
+++ lua/chavati/theme.lua
@@ -1,3 +1,11 @@
 local vim = vim
 vim.o.termguicolors = true
+vim.g.material_style = 'deep ocean'
+vim.g.material_italic_comments = true
+vim.g.material_italic_keywords = true
+vim.g.material_italic_functions = true
+vim.g.material_italic_variables = false
+vim.g.material_contrast = true
+vim.g.material_borders = false
+vim.g.material_disable_background = false
 require('material').set()
