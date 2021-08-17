964da44a10043790094b6488b73647f336fd6350
Salvador Hernandez Lopez <chavatloz@gmail.com>
2021-08-17 14:29:43 -0500

    Add support to tressitter on autopairs

--- lua/chavati/autopairs.lua
+++ lua/chavati/autopairs.lua
@@ -3,6 +3,12 @@ local npairs = require('nvim-autopairs')
 
 npairs.setup({
   disable_filetype = { "TelescopePrompt" , "vim" },
+  check_ts = true,
+    ts_config = {
+        lua = {'string'},-- it will not add pair on that treesitter node
+        javascript = {'template_string'},
+        java = false,-- don't check treesitter on java
+    }
 })
 
 require("nvim-autopairs.completion.compe").setup({
