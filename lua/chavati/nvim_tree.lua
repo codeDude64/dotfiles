23f3719123ccc99996536736ab785b7b38760169
Salvador Hernandez <salvador.hernandez@jibjab.com>
2021-10-01 11:43:45 -0500

    update tree

--- lua/chavati/nvim_tree.lua
+++ lua/chavati/nvim_tree.lua
@@ -1,20 +1,82 @@
+local vim = vim
 local tree = require "nvim-tree"
 
+vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
+vim.g.nvim_tree_gitignore = 1
+vim.g.nvim_tree_quit_on_open = 1
+vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_hide_dotfiles = 0
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_highlight_opened_files = 1
+vim.g.nvim_tree_root_folder_modifier = ':~'
+vim.g.nvim_tree_add_trailing = 1
+vim.g.nvim_tree_group_empty = 1
+vim.g.nvim_tree_disable_window_picker = 1
+vim.g.nvim_tree_icon_padding = ' '
+vim.g.nvim_tree_symlink_arrow = ' ➛ '
+vim.g.nvim_tree_respect_buf_cwd = 1
+vim.g.nvim_tree_create_in_closed_folder = 0
+vim.g.nvim_tree_refresh_wait = 500
+vim.g.nvim_tree_window_picker_exclude = {
+  filetype = {
+    'notify',
+    'packer',
+    'qf'
+  },
+  buftype = { 'terminal' }
+}
+
+vim.g.nvim_tree_special_files = { 'README.md', 'Makefile', 'MAKEFILE' }
+vim.g.nvim_tree_show_icons = {
+     git= 1,
+     folders= 0,
+     files= 0,
+     folder_arrows= 0,
+     }
+vim.g.nvim_tree_icons = {
+     default= '',
+     symlink= '',
+     git= {
+       unstaged= "✗",
+       staged= "✓",
+       unmerged= "",
+       renamed= "➜",
+       untracked= "★",
+       deleted= "",
+       ignored= "◌"
+       },
+     folder= {
+       arrow_open= "",
+       arrow_closed= "",
+       default= "",
+       open= "",
+       empty= "",
+       empty_open= "",
+       symlink= "",
+       symlink_open= "",
+       },
+       lsp= {
+         hint= "",
+         info= "",
+         warning= "",
+         error= "",
+       }
+     }
 
 tree.setup {
   disable_netrw       = true,
   hijack_netrw        = true,
   open_on_setup       = false,
   ignore_ft_on_setup  = {},
-  auto_close          = false,
+  auto_close          = true,
   open_on_tab         = false,
   update_to_buf_dir   = true,
-  hijack_cursor       = false,
-  update_cwd          = false,
-  lsp_diagnostics     = false,
+  hijack_cursor       = true,
+  update_cwd          = true,
+  lsp_diagnostics     = true,
   update_focused_file = {
-    enable      = false,
-    update_cwd  = false,
+    enable      = true,
+    update_cwd  = true,
     ignore_list = {}
   },
   system_open = {
@@ -32,6 +94,5 @@ tree.setup {
     }
   }
 }
-
 
 
