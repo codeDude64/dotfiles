f28913c282759b1ffadcde91c5d5851614996443
Salvador Hernandez <salvador.hernandez@jibjab.com>
2021-10-08 12:41:41 -0500

    changes on Treesitter

--- lua/chavati/nvim_tree.lua
+++ lua/chavati/nvim_tree.lua
@@ -70,7 +70,7 @@ tree.setup {
   ignore_ft_on_setup  = {},
   auto_close          = true,
   open_on_tab         = false,
-  update_to_buf_dir   = true,
+  update_to_buf_dir   = {enable= true, auto_open = true},
   hijack_cursor       = true,
   update_cwd          = true,
   lsp_diagnostics     = true,
