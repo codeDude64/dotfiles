df044ef7a330eab80d3e2ba94ce245c2fd312cfd
Salvador Hernandez Lopez <chavatloz@gmail.com>
2022-02-08 00:12:29 -0600

    use the material default set up

--- lua/chavati/theme.lua
+++ lua/chavati/theme.lua
@@ -3,29 +3,32 @@ local material = require "material"
 
 vim.o.termguicolors = true
 vim.g.material_style = 'darker'
-require("bufferline").setup{}
 vim.cmd[[colorscheme material]]
 
 material.setup({
-		contrast = true,
-		borders = false,
+		contrast = {
+			sidebars = false,
+			floating_windows = false,
+			line_numbers = false,
+			sign_column = false,
+			cursor_line = false,
+			non_current_windows = false,
+			popup_menu = false
+		},
 		italics = {
-			comments = true,
+			comments = false,
 			strings = false,
-			keywords = true,
-			functions = true,
+			keywords = false,
+			functions = false,
 			variables = false
 		},
-		contrast_windows = {
-			"terminal",
-			"packer",
-			"qf"
-		},
-		text_contrast = {
+		contrast_filetypes = {},
+		high_visibility = {
 			lighter = false,
 			darker = false
 		},
 		disable = {
+			borders = false,
 			background = true,
 			term_colors = false,
 			eob_lines = false
