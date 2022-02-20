bafaae69a7c6aab37ab48e31c0ebb74bda8cb4fa
Salvador Hernandez Lopez <chavatloz@gmail.com>
2022-02-19 21:29:36 -0600

    gitsigns

--- lua/chavati/gitsigns.lua
+++ lua/chavati/gitsigns.lua
@@ -1,6 +1,6 @@
 local gitsigns = require 'gitsigns'
 
-require('gitsigns').setup {
+gitsigns.setup {
   signs = {
     add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
     change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
