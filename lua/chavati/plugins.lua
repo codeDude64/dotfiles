90a946208afa41a4e7812fd6ef677655f4720131
Salvador Hernandez Lopez <chavatloz@gmail.com>
2022-07-15 01:14:54 -0500

    add kylechui/nvim-surround

--- lua/chavati/plugins.lua
+++ lua/chavati/plugins.lua
@@ -51,7 +51,7 @@ return packer.startup(function(use)
   use 'lewis6991/gitsigns.nvim'
 
   -- Surround
-  use 'tpope/vim-surround'
+  use 'kylechui/nvim-surround'
   -- Auto Pairs
   use 'windwp/nvim-autopairs'
   -- Tmux navigator
