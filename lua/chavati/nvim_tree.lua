3a43f0b786368db1db2848c2f125eaee26097fe3
Salvador Hernandez Lopez <chavatloz@gmail.com>
2022-07-16 17:14:15 -0500

    update the nvim_tree set up

--- lua/chavati/nvim_tree.lua
+++ lua/chavati/nvim_tree.lua
@@ -1,37 +1,21 @@
 local tree = require 'nvim-tree'
 
--- init.lua
-
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
 tree.setup {
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {},
-  auto_reload_on_write = true,
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  hijack_directories = {enable = true, auto_open = true},
-  diagnostics = {
-    enable = true,
-    icons = {hint = '', info = '', warning = '', error = ''}
-  },
-  update_focused_file = {enable = true, update_cwd = true, ignore_list = {}},
-  system_open = {cmd = nil, args = {}},
-  filters = {dotfiles = false, custom = {}},
-  git = {enable = true, ignore = true, timeout = 500},
+  sort_by = 'case_sensitive',
   view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = 'left',
-    mappings = {custom_only = false, list = {}},
-    number = false,
-    relativenumber = false,
-    signcolumn = 'yes'
+    adaptative_size = true,
+    mappings = {
+      list = {
+        {key = 'u', action = "dir_up"},
+      },
+    },
   },
-  trash = {cmd = 'trash', require_confirm = true},
-  actions = {change_dir = {global = false}, open_file = {quit_on_open = true}}
+  renderer = {
+    group_empty = true,
+  },
+
+  filters = {
+    dotfiles = false,
+  },
+
 }
