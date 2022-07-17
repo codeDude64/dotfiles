235fd55c3ba04355063b1d04ca5ba8650a5652a8
Salvador Hernandez Lopez <chavatloz@gmail.com>
2022-07-16 23:08:52 -0500

    fix typo

--- lua/chavati/nvim_tree.lua
+++ lua/chavati/nvim_tree.lua
@@ -3,7 +3,7 @@ local tree = require 'nvim-tree'
 tree.setup {
   sort_by = 'case_sensitive',
   view = {
-    adaptative_size = true,
+    adaptive_size = true,
     mappings = {
       list = {
         {key = 'u', action = "dir_up"},
