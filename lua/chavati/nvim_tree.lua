fd0301140eaccba64c6a9f7cda37a9b8bccde9be
Salvador Hernandez Lopez <chavatloz@gmail.com>
2022-07-18 12:03:42 -0500

    fix the update_focused_file

--- lua/chavati/nvim_tree.lua
+++ lua/chavati/nvim_tree.lua
@@ -2,20 +2,13 @@ local tree = require 'nvim-tree'
 
 tree.setup {
   sort_by = 'case_sensitive',
+  update_focused_file = {enable = true, update_cwd = true, ignore_list = {}},
   view = {
     adaptive_size = true,
-    mappings = {
-      list = {
-        {key = 'u', action = "dir_up"},
-      },
-    },
+    mappings = {list = {{key = 'u', action = "dir_up"}}}
   },
-  renderer = {
-    group_empty = true,
-  },
+  renderer = {group_empty = true},
 
-  filters = {
-    dotfiles = false,
-  },
+  filters = {dotfiles = false}
 
 }
