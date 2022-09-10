5988ca24dc9310f7537aad23728a378ae6da9320
codeDude <chavatloz@gmail.com>
2022-09-10 00:57:58 -0500

    use the directory that show the packer doc

--- lua/codeDude/plugins.lua
+++ lua/codeDude/plugins.lua
@@ -1,16 +1,21 @@
 local vim = vim
-local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
-local packer_bootstrap = nil
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
-    'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-    install_path
-  })
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') ..
+                         '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
+      'git', 'clone', '--depth', '1',
+      'https://github.com/wbthomason/packer.nvim', install_path
+    })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
-vim.cmd('packadd packer.nvim')
+local packer_bootstrap = ensure_packer()
 local packer = require 'packer'
 
 return packer.startup(function(use)
