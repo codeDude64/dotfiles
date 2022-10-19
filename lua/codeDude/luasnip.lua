ccf172c0e62b81261a80780a141768ae86a99ae3
codeDude <chavatloz@gmail.com>
2022-10-19 12:58:45 -0500

    basic luasnip configuration

---
+++ lua/codeDude/luasnip.lua
@@ -0,0 +1,15 @@
+local ls = require 'luasnip'
+local types = require 'luasnip.util.types'
+
+ls.config.set_config {
+  history = true,
+  updateEvents = "TextChanged,TextChangedI",
+  enable_autosnippets = true,
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = { { "←", "Error" } }
+      }
+    }
+  }
+}
