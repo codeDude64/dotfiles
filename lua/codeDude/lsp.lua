299887e888eb9f4188b2555201ce4cbb1b0c82d0
codeDude <chavatloz@gmail.com>
2022-10-19 11:43:55 -0500

    install luasnip and attach the lusnip config on the cmp one

--- lua/codeDude/lsp.lua
+++ lua/codeDude/lsp.lua
@@ -1,9 +1,7 @@
 local vim = vim
 local lspconfig = require 'lspconfig'
-local cmp_nvim_lsp = require 'cmp_nvim_lsp'
-local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol
-  .make_client_capabilities())
-
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local function default_on_attach(client) print('Attaching to ' .. client.name) end
 
 local default_config = {
