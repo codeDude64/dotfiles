aed3a3b4aa9ff9a5418a8165643f0a5202cd1faf
codeDude <chavatloz@gmail.com>
2022-11-01 00:02:48 -0600

    remove log

--- lua/codeDude/lsp/init.lua
+++ lua/codeDude/lsp/init.lua
@@ -3,7 +3,6 @@ local lsp_servers = require 'codeDude.lsp.lsp_servers'
 
 local function load_lsp_servers()
   for key, server in pairs(lsp_servers) do
-    print("hola")
     lspconfig[key].setup(server)
   end
 
