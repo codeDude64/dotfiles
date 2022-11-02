e4deafb31cef4eb82008dc0f9c765c9b20237bdc
codeDude <chavatloz@gmail.com>
2022-11-02 00:50:32 -0600

    add git diff file mapping

--- lua/codeDude/mappings/git.lua
+++ lua/codeDude/mappings/git.lua
@@ -1,5 +1,6 @@
 local neogit = require 'neogit'
 
 return {
-  { 'n', '<leader>gs', function() neogit.open() end }
+  { 'n', '<leader>gs', function() neogit.open() end },
+  { 'n', '<leader>hf', function() vim.cmd("DiffviewFileHistory " .. vim.api.nvim_buf_get_name(0)) end }
 }
