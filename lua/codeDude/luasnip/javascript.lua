8d393b74647cd3f48e72384d582bf21944b31c5a
codeDude <chavatloz@gmail.com>
2022-11-03 12:30:04 -0600

    improve the strings on js snipets

--- lua/codeDude/luasnip/javascript.lua
+++ lua/codeDude/luasnip/javascript.lua
@@ -7,13 +7,13 @@ local sn = ls.snippet_node
 local fmt = require 'luasnip.extras.fmt'.fmt
 
 return {
-  s("cl", fmt([[console.log({});]],
+  s('cl', fmt([[console.log({});]],
     c(1, {
       sn(1, { t('"'), i(1), t('"') }),
       sn(1, { t('"'), i(1), t('"'), t(', '), i(2) }),
       sn(1, { t('{'), i(1), t('}') })
     })
   )),
-  s("cg",
-    fmt("console.group(\"{}\");\nconsole.log(\"{}\");\nconsole.groupEnd();", { i(1, "groupName"), i(2, "message") }))
+  s('cg',
+    fmt('console.group("{}");\nconsole.log("{}");\nconsole.groupEnd();', { i(1, 'groupName'), i(2, 'message') }))
 }
