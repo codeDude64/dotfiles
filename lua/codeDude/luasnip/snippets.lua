413ac0635013d6ef27206f018c9fdd73f4e80bf6
codeDude <codeDude64@proton.me>
2022-11-11 19:05:52 -0600

    add cpp snippets (#38)

--- lua/codeDude/luasnip/snippets.lua
+++ lua/codeDude/luasnip/snippets.lua
@@ -1,9 +1,11 @@
 local all = require 'codeDude.luasnip.all'
 local javascript = require 'codeDude.luasnip.javascript'
+local cpp = require 'codeDude.luasnip.cpp'
 
 return {
   all = all,
   javascript = javascript,
   typescript = javascript,
-  typescriptreact = javascript
+  typescriptreact = javascript,
+  cpp = cpp
 }
