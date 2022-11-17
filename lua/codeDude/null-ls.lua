1057753e19a257d3fe24dc2f3c16bb43c16dc587
codeDude <chavatloz@gmail.com>
2022-11-17 14:59:01 -0600

    add prettier formatting

--- lua/codeDude/null-ls.lua
+++ lua/codeDude/null-ls.lua
@@ -6,5 +6,6 @@ null_ls.setup({
     null_ls.builtins.formatting.eslint,
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.code_actions.eslint,
+    null_ls.builtins.formatting.prettierd,
   },
 })
