045c3901633edd0a06eecd4ed2999a6d0ca5f55f
codeDude <chavatloz@gmail.com>
2022-11-17 14:48:18 -0600

    add code actions to eslint

--- lua/codeDude/null-ls.lua
+++ lua/codeDude/null-ls.lua
@@ -5,6 +5,6 @@ null_ls.setup({
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.eslint,
     null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.completion.spell,
+    null_ls.builtins.code_actions.eslint,
   },
 })
