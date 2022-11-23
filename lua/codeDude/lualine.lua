d98c719c2edf024d42668e67187ea7cb0f37ff2b
codeDude <chavatloz@gmail.com>
2022-11-22 21:51:43 -0600

    create the funtion to the scroll lualine component

--- lua/codeDude/lualine.lua
+++ lua/codeDude/lualine.lua
@@ -1,5 +1,26 @@
 local lualine = require 'lualine'
 
+local function scrollbar()
+  local current_line = vim.fn.line('.')
+  local total_lines = vim.fn.line('$')
+  local default_chars = { '  ', '__', '▁▁', '▂▂', '▃▃', '▄▄', '▅▅', '▆▆', '▇▇', '██' }
+  local index = 1
+
+  if current_line == 1 then
+    index = 1
+  elseif current_line == total_lines then
+    index = #default_chars
+  else
+    local line_no_fraction = vim.fn.floor(current_line) / vim.fn.floor(total_lines)
+    index = vim.fn.float2nr(line_no_fraction * #default_chars)
+    P(index)
+    if index == 0 then
+      index = 1
+    end
+  end
+  return default_chars[index]
+end
+
 lualine.setup {
   options = {
     icons_enabled = true,
@@ -24,7 +45,7 @@ lualine.setup {
     lualine_b = { 'branch', 'diff', 'diagnostics' },
     lualine_c = { 'filename' },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
-    lualine_y = { 'progress' },
+    lualine_y = { 'progress', scrollbar },
     lualine_z = { 'location' }
   },
   inactive_sections = {
