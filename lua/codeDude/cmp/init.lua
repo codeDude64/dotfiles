48301a8222f36d4d486a11214da6a0334e760871
codeDude <codeDude64@proton.me>
2023-01-07 22:18:30 -0600

    add the vscode icons (#53)
    * add the vscode icons
    
    * change the completation on cmd mode
    
    * change the completation in cmp
    
    * add the border in the documentation

--- lua/codeDude/cmp/init.lua
+++ lua/codeDude/cmp/init.lua
@@ -3,16 +3,17 @@ if not status_cmp_ok then
   vim.notify('Error: cmp plugin is NIL')
   return
 end
-local lspkind = require 'lspkind'
 local luasnip = require 'luasnip'
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp_kinds = require 'codeDude.cmp.icon_kinds'
 
 cmp.setup({
   completion = {
     completeopt = 'menu,menuone,noinsert'
   },
   window = {
-    completion = cmp.config.window.bordered({ border = 'double' })
+    completion = cmp.config.window.bordered({ border = 'double' }),
+    documentation = cmp.config.window.bordered({ col_offset = 5 })
   },
   sorting = {
     comparators = {
@@ -70,6 +71,7 @@ cmp.setup({
     { name = 'cmd_line' }
   },
   formatting = {
+    fields = { 'kind', 'abbr', 'menu' },
     format = function(entry, vim_item)
       if vim.tbl_contains({ 'path' }, entry.source.name) then
         local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
@@ -79,7 +81,8 @@ cmp.setup({
           return vim_item
         end
       end
-      return lspkind.cmp_format({ with_text = true })(entry, vim_item)
+      vim_item.kind = (cmp_kinds[vim_item.kind] or '') .. vim_item.kind
+      return vim_item
     end
   },
   map_cr = true,
@@ -96,6 +99,9 @@ cmp.setup.filetype('gitcommit', {
 })
 
 cmp.setup.cmdline({ '/', '?' }, {
+  completion = {
+    completeopt = 'menu,menuone,noselect,noinsert'
+  },
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'buffer' }
@@ -103,6 +109,9 @@ cmp.setup.cmdline({ '/', '?' }, {
 })
 
 cmp.setup.cmdline(':', {
+  completion = {
+    completeopt = 'menu,menuone,noselect,noinsert'
+  },
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = 'path' }
