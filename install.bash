1949d03c0f2c6d7353d0c8155ba19bacf5f9e437
codeDude <chavatloz@gmail.com>
2023-01-08 20:10:39 -0600

    add the instalation of wezterm

--- install.bash
+++ install.bash
@@ -3,12 +3,14 @@
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 export CONF_DIR=$HOME/.config
 export ALACRITTY_DIR=$CONF_DIR/alacritty
+export WEZTERM_DIR=$CONF_DIR/wezterm
 export NVIM_DIR=$CONF_DIR/nvim
 export DOTFILES_DIR=$HOME/dotfiles
 export NVIM_CACHE_DIR=$HOME/.cache/nvim
 
 rm -rf $NVIM_DIR/init.lua
 rm -rf $ALACRITTY_DIR/alacritty.yml
+rm -rf $WEZTERM_DIR/wezterm.lua
 rm -rf ~/.zshrc
 rm -rf ~/.tmux.conf
 rm -rf $NVM_DIR/default-packages
@@ -17,6 +19,8 @@ rm -rf $CONF_DIR/lua-format.yml
 
 mkdir -p $NVIM_DIR
 mkdir -p $NVM_DIR
+mkdir -p $ALACRITTY_DIR
+mkdir -p $WEZTERM_DIR
 
 
 ln -s $DOTFILES_DIR/.zshrc $HOME/.zshrc
@@ -29,3 +33,4 @@ ln -s $DOTFILES_DIR/lua-format.yml $CONF_DIR/lua-format.yml
 ln -s $DOTFILES_DIR/init.lua $NVIM_DIR/init.lua
 ln -s $DOTFILES_DIR/lua $NVIM_DIR/lua
 ln -s $DOTFILES_DIR/alacritty.yml $ALACRITTY_DIR/alacritty.yml
+ln -s $DOTFILES_DIR/wezterm.lua $WEZTERM_DIR/wezterm.lua
