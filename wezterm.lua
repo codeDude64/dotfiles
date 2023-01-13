fb5139a25fb09572b61d6a7a4073548b849c10e7
codeDude <chavatloz@gmail.com>
2023-01-13 01:28:54 -0600

    add bold font

--- wezterm.lua
+++ wezterm.lua
@@ -3,5 +3,5 @@ local wezterm = require'wezterm'
 return {
   color_scheme = 'palenight (Gogh)',
   window_background_opacity = .90,
-  font = wezterm.font 'VictorMono Nerd Font',
+  font = wezterm.font ( 'VictorMono Nerd Font', { weight = 'Bold' } ),
 }
