3f696e3dc8fda8a7c240d7b866c1ff994f6e1b19
codeDude <chavatloz@gmail.com>
2023-03-19 13:22:56 -0600

    telescope

--- lua/keymaps/telescope.lua
+++ lua/keymaps/telescope.lua
@@ -2,14 +2,14 @@ local telescope = require 'telescope'
 local telescope_builtin = require 'telescope.builtin'
 
 return {
-  { 'n', '<c-s>', function() telescope_builtin.grep_string { search = vim.fn.expand('<cword>') } end },
-  { 'n', '<s-s>', function() telescope_builtin.grep_string({ search = vim.fn.input('Grep For > ') }) end },
-  { 'n', '<c-p>', function() telescope_builtin.git_files() end },
-  { 'n', '<s-p>', function() telescope_builtin.find_files({ no_ignore = true }) end },
-  { 'n', 'gl', function() telescope_builtin.git_commits() end },
-  { 'n', 'gb', function() telescope_builtin.git_branches() end },
-  { 'n', '<leader>ts', function() telescope_builtin.treesitter() end },
-  { 'n', '<leader>to', function() telescope_builtin.builtin() end },
-  { 'n', '<leader><c-p>', function() telescope.extensions.repo.list {} end },
-  { 'n', '<leader><c-n>', function() telescope.extensions.node_modules.list {} end }
+  { '<c-s>',         function() telescope_builtin.grep_string { search = vim.fn.expand('<cword>') } end },
+  { '<s-s>',         function() telescope_builtin.grep_string({ search = vim.fn.input('Grep For > ') }) end },
+  { '<c-p>',         function() telescope_builtin.git_files() end },
+  { '<s-p>',         function() telescope_builtin.find_files({ no_ignore = true }) end },
+  { 'gl',            function() telescope_builtin.git_commits() end },
+  { 'gb',            function() telescope_builtin.git_branches() end },
+  { '<leader>ts',    function() telescope_builtin.treesitter() end },
+  { '<leader>to',    function() telescope_builtin.builtin() end },
+  { '<leader><c-p>', function() telescope.extensions.repo.list {} end },
+  { '<leader><c-n>', function() telescope.extensions.node_modules.list {} end }
 }
