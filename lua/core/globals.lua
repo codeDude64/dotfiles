c530316583d998d19d0e946f158b2e9f084e5124
codeDude <chavatloz@gmail.com>
2023-03-20 19:17:19 -0600

    add the globals functions

---
+++ lua/core/globals.lua
@@ -0,0 +1,12 @@
+RELOAD = function(...) require 'plenary.reload'.reload_module(...) end
+
+R = function(name)
+  print('Reloading config...')
+  RELOAD(name)
+  print('Done :)')
+  return require(name)
+end
+
+RCONFIG = function() R('codeDude') end
+
+P = function(table) print(vim.inspect(table)) end
--- lua/core/init.lua
+++ lua/core/init.lua
@@ -3,6 +3,7 @@ local load_core = function ()
   require("core.options.global")
   require("core.options.buffer")
   require("core.options.window")
+  require("core.globals")
   require("core.keymaper")
   require("core.lazy")
 end
