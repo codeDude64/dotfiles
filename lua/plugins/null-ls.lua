2394bfd7d3302518c46d1b394403cbaff09f2326
codeDude <codeDude64@proton.me>
2023-04-16 21:54:53 -0600

    add eslint_d and add the debug mode (#66)

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,16 +1,17 @@
 return {
-  'jose-elias-alvarez/null-ls.nvim',
+  "jose-elias-alvarez/null-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
 
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.eslint,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.code_actions.eslint,
-        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.formatting.eslint_d,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.code_actions.eslint_d,
       },
+      debug = true,
     })
-  end
+  end,
 }
