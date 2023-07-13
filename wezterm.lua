b16170943a7c7c079938edaf44001bb68fca5049
codeDude <chavatloz@gmail.com>
2023-07-13 00:37:25 -0600

    :art: improve the transparency of wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,6 @@ local wezterm = require'wezterm'
 
 return {
   color_scheme = 'palenight (Gogh)',
-  window_background_opacity = .95,
+  window_background_opacity = .85,
   font = wezterm.font ( 'VictorMono Nerd Font', { weight = 'Bold' } ),
 }
