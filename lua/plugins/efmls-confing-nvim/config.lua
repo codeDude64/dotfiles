15496141eea8438ef914a6baed5e201564ef0b97
codeDude <chavatloz@gmail.com>
2023-08-11 00:23:09 -0600

    - Add rubocop

--- lua/plugins/efmls-confing-nvim/config.lua
+++ lua/plugins/efmls-confing-nvim/config.lua
@@ -1,7 +1,8 @@
 local eslint_d = require 'efmls-configs.linters.eslint_d'
 local eslint_d_formatter = require 'efmls-configs.formatters.eslint_d'
 local stylua = require 'efmls-configs.formatters.stylua'
-local luacheck = require('efmls-configs.linters.luacheck')
+local luacheck = require 'efmls-configs.linters.luacheck'
+local rubocop = require 'efmls-configs.linters.rubocop'
 
 local js_languages = {
   linter = eslint_d,
@@ -17,4 +18,7 @@ return {
     formatter = stylua,
     linter = luacheck,
   },
+  ruby = {
+    linter = rubocop
+  }
 }
