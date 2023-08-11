3b5c986aaa51022f821088e50387637dd3714654
codeDude <chavatloz@gmail.com>
2023-08-11 00:18:15 -0600

    - Add lua formatter and linter

--- lua/plugins/efmls-confing-nvim/config.lua
+++ lua/plugins/efmls-confing-nvim/config.lua
@@ -1,5 +1,7 @@
 local eslint_d = require 'efmls-configs.linters.eslint_d'
 local eslint_d_formatter = require 'efmls-configs.formatters.eslint_d'
+local stylua = require 'efmls-configs.formatters.stylua'
+local luacheck = require('efmls-configs.linters.luacheck')
 
 local js_languages = {
   linter = eslint_d,
@@ -10,5 +12,9 @@ return {
   javascript = js_languages,
   javascriptreact = js_languages,
   typescript = js_languages,
-  typescriptreact = js_languages
+  typescriptreact = js_languages,
+  lua = {
+    formatter = stylua,
+    linter = luacheck,
+  },
 }
