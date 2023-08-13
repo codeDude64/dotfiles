79d65a2422a5f8f39e97fa4e70e1b911d1bd653a
codeDude <codeDude64@proton.me>
2023-08-13 10:22:58 -0600

    - Remove clang-tidy (#85)

--- lua/plugins/efmls-confing-nvim/config.lua
+++ lua/plugins/efmls-confing-nvim/config.lua
@@ -3,18 +3,10 @@ local eslint_d_formatter = require 'efmls-configs.formatters.eslint_d'
 local stylua = require 'efmls-configs.formatters.stylua'
 local luacheck = require 'efmls-configs.linters.luacheck'
 local rubocop = require 'efmls-configs.linters.rubocop'
-local clang_tidy = require 'efmls-configs.linters.clang_tidy'
-local clang_tidy_formatter = require 'efmls-configs.formatters.clang_tidy'
 
 local js_languages = {
   linter = eslint_d,
   formatter = eslint_d_formatter,
-}
-
-local c_languages = {
-  linter = clang_tidy,
-  formatter = clang_tidy_formatter,
-
 }
 
 return {
@@ -28,7 +20,5 @@ return {
   },
   ruby = {
     linter = rubocop
-  },
-  cpp = c_languages,
-  c = c_languages
+  }
 }
