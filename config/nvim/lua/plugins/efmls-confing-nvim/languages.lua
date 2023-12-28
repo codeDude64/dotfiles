2d6576a891d73a8377b3275821f20b4a2b1532c3
codeDude <chavatloz@gmail.com>
2023-12-28 10:43:52 -0600

    bugfix/fix-solargraph-and-lua-ls-issues - Remove the efm linters and formats of ruby and lua - Run the bundle install cmd in the ruby projects - Update the lua_ls config like the documentation suggest

--- config/nvim/lua/plugins/efmls-confing-nvim/languages.lua
+++ config/nvim/lua/plugins/efmls-confing-nvim/languages.lua
@@ -1,8 +1,5 @@
 local eslint_d = require 'efmls-configs.linters.eslint_d'
 local eslint_d_formatter = require 'efmls-configs.formatters.eslint_d'
-local stylua = require 'efmls-configs.formatters.stylua'
-local luacheck = require 'efmls-configs.linters.luacheck'
-local rubocop = require 'efmls-configs.linters.rubocop'
 
 local js_languages = { eslint_d, eslint_d_formatter }
 
@@ -11,6 +8,4 @@ return {
   javascriptreact = js_languages,
   typescript = js_languages,
   typescriptreact = js_languages,
-  lua = { stylua, luacheck },
-  ruby = { rubocop }
 }
