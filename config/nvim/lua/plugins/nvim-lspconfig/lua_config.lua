5bffd6ad94d66ac508b206a848070491589ab60a
codeDude <codeDude64@proton.me>
2023-12-28 10:46:29 -0600

    bugfix/fix-solargraph-and-lua-ls-issues (#99)
    - Remove the efm linters and formats of ruby and lua
    - Run the bundle install cmd in the ruby projects
    - Update the lua_ls config like the documentation suggest

--- config/nvim/lua/plugins/nvim-lspconfig/lua_config.lua
+++ config/nvim/lua/plugins/nvim-lspconfig/lua_config.lua
@@ -1,19 +1,23 @@
-local default_config = require 'plugins.nvim-lspconfig.default_config'
+local default_config = require("plugins.nvim-lspconfig.default_config")
 
-return  {
+return {
   on_attach = default_config.on_attach,
   capabilities = default_config.capabilities,
-  settings = {
-    Lua = {
-      runtime = { version = 'LuaJIT' },
-      diagnostics = { globals = { 'vim' } },
-      workspace = {
-        maxPreload = 10000,
-        library = vim.api.nvim_get_runtime_file('', true),
-        checkThirdParty = false,
-      },
-      telemetry = { enable = false }
-    }
-  }
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+      client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+        Lua = {
+          runtime = { version = "LuaJIT" },
+          workspace = {
+            checkThirdParty = false,
+            library = { vim.env.VIMRUNTIME },
+          },
+          telemetry = { enable = false },
+        },
+      })
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end,
 }
-
