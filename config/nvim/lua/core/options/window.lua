5a94982ad23f771d25cbda7416284fd22c9fe4a2
codeDude <codeDude64@proton.me>
2024-03-06 00:18:09 -0600

    -Add emojis signs (#103)

--- config/nvim/lua/core/options/window.lua
+++ config/nvim/lua/core/options/window.lua
@@ -7,3 +7,9 @@ local options = {
 for name, value in pairs(options) do
   vim.wo[name] = value
 end
+
+local signs = { Error = "🤬", Warn = "🧐", Hint = "🎯", Info = "🤓" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
