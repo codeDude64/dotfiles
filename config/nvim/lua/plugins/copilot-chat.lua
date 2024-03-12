8ca20aa43291d321c48ad4c4f789755d724c747e
codeDude <codeDude64@proton.me>
2024-03-12 00:28:40 -0600

    Feature/copilot change repo (#105)
    * copilot-change-repo
    - Change the repo of copilot
    - add the configuration
    - remove the on_attach function
    
    * copilot-change-repo
    - Add a keymaps file to copilot-chat
    - Remove the comments from the copilot-chat plugin file
    - Add the keymaps to the copilot-chat plugin file
    
    * copilot-change-repo
    - Remove lines

--- config/nvim/lua/plugins/copilot-chat.lua
+++ config/nvim/lua/plugins/copilot-chat.lua
@@ -1,4 +1,18 @@
 return {
-  'gptlang/CopilotChat.nvim',
-  dependencies = 'zbirenbaum/copilot.lua',
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      show_help = "yes",
+      debug = false,
+      disable_extra_info = 'no',
+      language = "English"
+    },
+    build = function()
+      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+    end,
+    event = "VeryLazy",
+    keys = function()
+      return require("keymaps.copilot-chat")
+    end
+  },
 }
