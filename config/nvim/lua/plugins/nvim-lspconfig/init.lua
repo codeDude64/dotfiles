4a41d91400c3702dc34b3c7fd6fef21e64761b85
codeDude <codeDude64@proton.me>
2024-04-28 01:51:46 -0600

    feature/migration-fedora (#113)
    * feature/migration-fedora
    Fix #112
    - fedora migration stuff
    
    * feature/migration-fedora
    - add mason
    
    * feature/migration-fedora
    - Remove double config of mason
    
    ---------
    
    Co-authored-by: Salvador Hernandez Lopez <salvador.hernandez@jibjab.com>

--- config/nvim/lua/plugins/nvim-lspconfig/init.lua
+++ config/nvim/lua/plugins/nvim-lspconfig/init.lua
@@ -5,12 +5,18 @@ return {
     return require("keymaps.nvim-lspconfig")
   end,
   dependencies = {
+  { 'williamboman/mason.nvim', config = true },
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
     'hrsh7th/nvim-cmp',
     "onsails/lspkind-nvim",
   },
   config = function()
     local lspconfig   = require 'lspconfig'
     local lsp_servers = require 'plugins.nvim-lspconfig.lsp_servers'
+    local mason_lspconfig = require 'mason-lspconfig'
+    
+    mason_lspconfig.setup{ automatic_installation = true }
 
     local function load_lsp_servers()
       for server, config in pairs(lsp_servers) do
--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh +x
 
 export CONF_DIR=$HOME/.config
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 
 export DOTFILES_DIR=$HOME/dotfiles
 
@@ -16,7 +15,6 @@ remove_file_if_exists() {
 config_files=(
   $DOTFILES_DIR/config/nvim/lua
   $DOTFILES_DIR/config/nvim/init.lua
-  $DOTFILES_DIR/config/wezterm/wezterm.lua
 )
 
 home_files=(
@@ -27,10 +25,8 @@ home_files=(
 files_to_remove=(
   $CONF_DIR/nvim/lua
   $CONF_DIR/nvim/init.lua
-  $CONF_DIR/wezterm/wezterm.lua
   $HOME/.zshrc
   $HOME/.tmux.conf
-  $NVM_DIR/default-packages
 )
 
 for file in "${files_to_remove[@]}"; do
@@ -54,6 +50,4 @@ for file in "${home_files[@]}"; do
   echo "$HOME/$(basename "$file") added successfully"
 done
 
-ln -s $DOTFILES_DIR/nvm/default-packages $NVM_DIR/default-packages
-echo "$NVM_DIR/default-packages added successfully"
 
