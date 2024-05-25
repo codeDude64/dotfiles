dd9abbb4a8b59e63faba9fae541e23c2bb28e4ba
codeDude <codeDude64@proton.me>
2024-05-24 22:10:29 -0600

    bugfix/efmls-doesnt-load-by-mason Fix #114 - Setup efmls in the lspconfig context - Setup mason-tool-installer in the lspconfig context

--- config/nvim/lua/plugins/mason-tool-installer.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "WhoIsSethDaniel/mason-tool-installer.nvim",
-  config = function()
-    local mason_tool_installer = require("mason-tool-installer")
-    mason_tool_installer.setup{ ensure_installed = ensure_installed }
-  end
-}
--- config/nvim/lua/plugins/nvim-lspconfig/init.lua
+++ config/nvim/lua/plugins/nvim-lspconfig/init.lua
@@ -5,18 +5,22 @@ return {
     return require("keymaps.nvim-lspconfig")
   end,
   dependencies = {
-  { 'williamboman/mason.nvim', config = true },
+    { 'williamboman/mason.nvim', config = true },
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     'hrsh7th/nvim-cmp',
     "onsails/lspkind-nvim",
+    "creativenull/efmls-configs-nvim"
   },
   config = function()
-    local lspconfig   = require 'lspconfig'
-    local lsp_servers = require 'plugins.nvim-lspconfig.lsp_servers'
+    local lspconfig       = require 'lspconfig'
+    local lsp_servers     = require 'plugins.nvim-lspconfig.lsp_servers'
+    local mason           = require 'mason'
     local mason_lspconfig = require 'mason-lspconfig'
-    
-    mason_lspconfig.setup{ automatic_installation = true }
+    local mason_tool_installer = require("mason-tool-installer")
+    local default_config  = require('plugins.nvim-lspconfig.default_config')
+    local languages       = require 'plugins.efmls-confing-nvim.languages'
+
 
     local function load_lsp_servers()
       for server, config in pairs(lsp_servers) do
@@ -24,6 +28,29 @@ return {
       end
     end
 
+    local function setup_efmls()
+      local efmls_config = {
+        filetypes = vim.tbl_keys(languages),
+        settings = {
+          rootMarkers = { '.git/' },
+          languages = languages,
+        },
+        init_options = {
+          documentFormatting = true,
+          documentRangeFormatting = true,
+        },
+      }
+
+      local efml_configs = vim.tbl_extend('force', efmls_config, default_config)
+
+      lspconfig.efm.setup(efml_configs)
+    end
+
+    mason.setup()
+    mason_tool_installer.setup{ automatic_installation = true }
+    mason_lspconfig.setup { automatic_installation = true }
+
     load_lsp_servers()
+    setup_efmls()
   end
 }
