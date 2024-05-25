f9fd8a063a8f228b07abec29e8e5737d93aa4ea7
codeDude <codeDude64@proton.me>
2024-05-24 22:26:07 -0600

    bugfix/efmls-doesnt-load-by-mason - use solargraph custom setup

--- config/nvim/lua/plugins/nvim-lspconfig/lsp_servers.lua
+++ config/nvim/lua/plugins/nvim-lspconfig/lsp_servers.lua
@@ -2,6 +2,7 @@ local json_config    = require('plugins.nvim-lspconfig.json_config')
 local lua_config     = require('plugins.nvim-lspconfig.lua_config')
 local ccls_config    = require('plugins.nvim-lspconfig.ccls_config')
 local default_config = require('plugins.nvim-lspconfig.default_config')
+local solargraph_config = require('plugins.nvim-lspconfig.solargraph_config')
 
 return {
   tsserver = default_config,
@@ -14,7 +15,7 @@ return {
   yamlls = default_config,
   vimls = default_config,
   dockerls = default_config,
-  solargraph = default_config,
+  solargraph = solargraph_config,
   cmake = default_config,
   lemminx = default_config,
 
