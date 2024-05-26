3fffaf1b1dbb6acef5dab7d6dacd7dd085411f9f
codeDude <codeDude64@proton.me>
2024-05-25 23:56:16 -0600

    feature/update-copilot-stuff - Update copilot and copilot-cmp

--- config/nvim/lua/plugins/nvim-cmp/init.lua
+++ config/nvim/lua/plugins/nvim-cmp/init.lua
@@ -23,11 +23,9 @@ return {
     local cmp_kinds = require("plugins.nvim-cmp.icon_kinds")
 
     local has_words_before = function()
-      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-        return false
-      end
+      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
     end
 
     cmp.setup({
