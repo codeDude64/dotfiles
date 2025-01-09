edbb92b5788bd6816d3f01153754df3f063d45e4
codeDude <codeDude@disroot.org>
2025-01-09 10:57:41 -0600

    Allow auto formatting
    - create a group and a command to allow the auto formatting
    - Return to vim tmux plugin (because conflicts)

--- config/nvim/lua/plugins/nvim-lspconfig/setups/efmls.lua
+++ config/nvim/lua/plugins/nvim-lspconfig/setups/efmls.lua
@@ -18,6 +18,20 @@ local function setup_efmls()
   local efml_configs = vim.tbl_extend('force', efmls_config, default_config)
 
   lspconfig.efm.setup(efml_configs)
+
+  local lsp_fmt_group = vim.api.nvim_create_augroup('LspFormattingGroup', {})
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    group = lsp_fmt_group,
+    callback = function(ev)
+      local efm = vim.lsp.get_clients({ name = 'efm', bufnr = ev.buf })
+
+      if vim.tbl_isempty(efm) then
+        return
+      end
+
+      vim.lsp.buf.format({ name = 'efm' })
+    end,
+  })
 end
 
 return setup_efmls
