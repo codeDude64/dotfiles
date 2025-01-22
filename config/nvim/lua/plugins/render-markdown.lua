d13ef1efec73dd1b90660e6031a093131c673351
codeDude <codeDude@disroot.org>
2025-01-21 18:32:50 -0600

    remove comments

--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -1,6 +1,6 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },    -- if you prefer nvim-web-devicons
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   ft = "markdown",
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
