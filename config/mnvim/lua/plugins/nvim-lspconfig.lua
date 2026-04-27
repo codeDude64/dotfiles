93e333f2b040fda554bdfc5f380bbf5d0f95dfc4
codeDude <codeDude@disroot.org>
2026-04-26 22:13:27 -0600

    add keymaps to lsp

--- config/mnvim/lua/plugins/nvim-lspconfig.lua
+++ config/mnvim/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,43 @@ vim.pack.add({
   { src = 'https://github.com/neovim/nvim-lspconfig' }
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(event)
+    local opts = { buffer = event.buf }
+
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+
+    vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+
+    vim.keymap.set('n', 'sd', function()
+      vim.diagnostic.open_float({ scope = 'line', bufnr = opts.buffer })
+    end, opts)
+
+    vim.keymap.set('n', '<leader>sd', function()
+      vim.diagnostic.open_float({ scope = 'cursor', bufnr = opts.buffer })
+    end, opts)
+
+    vim.keymap.set('n', '[e', function() vim.diagnostic.jump({ count = -1 }) end, opts)
+    vim.keymap.set('n', ']e', function() vim.diagnostic.jump({ count = 1 }) end, opts)
+
+    vim.keymap.set('n', '[E', function()
+      vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.ERROR })
+    end, opts)
+
+    vim.keymap.set('n', ']E', function()
+      vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.ERROR })
+    end, opts)
+
+    vim.keymap.set('n', '<C-f>', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
